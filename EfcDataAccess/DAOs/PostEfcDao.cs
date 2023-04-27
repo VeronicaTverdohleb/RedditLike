1c70d1b24863e944cf3642e4d143cce4249c6101
VeronicaTverdohleb <101176218+VeronicaTverdohleb@users.noreply.github.com>
2023-04-27 19:02:15 +0200

    datbase

---
+++ EfcDataAccess/DAOs/PostEfcDao.cs
@@ -0,0 +1,79 @@
+﻿using Application.DaoInterfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace EfcDataAccess.DAOs;
+
+public class PostEfcDao: IPostDao
+{
+    private readonly PostContext context;
+    
+
+    public PostEfcDao(PostContext context)
+    {
+        this.context = context;
+    }
+    public async Task<Post> CreateAsync(Post post)
+    {
+        EntityEntry<Post> added = await context.Posts.AddAsync(post);
+        await context.SaveChangesAsync();
+        return added.Entity;
+    }
+
+    public async Task<IEnumerable<Post>> GetAsync(SearchPostParametersDto searchParameters)
+    {
+        IQueryable<Post> query = context.Posts.Include(post => post.Owner).AsQueryable();
+        
+        if (!string.IsNullOrEmpty(searchParameters.Username))
+        {
+            // we know username is unique, so just fetch the first
+            query = query.Where(post =>
+                post.Owner.UserName.ToLower().Equals(searchParameters.Username.ToLower()));
+        }
+        
+        if (searchParameters.UserId != null)
+        {
+            query = query.Where(t => t.Owner.Id == searchParameters.UserId);
+        }
+        
+        if (!string.IsNullOrEmpty(searchParameters.TitleContains))
+        {
+            query = query.Where(t =>
+                t.Title.ToLower().Contains(searchParameters.TitleContains.ToLower()));
+        }
+
+        List<Post> result = await query.ToListAsync();
+        return result;
+         }
+
+    public async Task UpdateAsync(Post post)
+    {
+        context.Posts.Update(post);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<Post> GetByIdAsync(int id)
+    {
+        Post? found = await context.Posts
+            .AsNoTracking()
+            .Include(post => post.Owner)
+            .SingleOrDefaultAsync(post => post.Id == id);
+        return found;
+       
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        Post? existing = await GetByIdAsync(id);
+        if (existing == null)
+        {
+            throw new Exception($"Todo with id {id} not found");
+        }
+
+        context.Posts.Remove(existing);
+        await context.SaveChangesAsync();
+        
+    }
+}
